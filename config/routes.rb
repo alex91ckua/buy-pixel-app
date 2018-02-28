b519070189a75a7cb28cac50838fd29e60e8c1de
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-01 00:07:38 +0200

    Checkout success page added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :cart, only: [:show, :create]
   root 'home#index'
   resource :charge, only: [:new, :create]
+  get 'charge/success', to: 'charges#success'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
