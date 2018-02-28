c324e45dabd16081b0914c8d1499c775e440f9e0
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-01 00:07:01 +0200

    Total and qty moved to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def total_amount
+    # in cents
+    session[:qty] * 100
+  end
+  helper_method :total_amount
+
+  def product_qty
+    session[:qty]
+  end
+  helper_method :product_qty
+
 end
