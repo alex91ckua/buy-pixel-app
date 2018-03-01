6aba9ada0c8488db4d881f4202233b974fc9a90e
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-01 15:06:09 +0200

    total calculation added to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,9 +3,23 @@ class Order < ApplicationRecord
   has_many :order_items
   before_validation :set_order_status, on: :create
   before_save :update_subtotal
+  before_save :update_total
+  before_save :update_total_cents
 
   def subtotal
     order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
+  end
+
+  def total
+    tax_percent = 0 # Tax in percent
+    if tax_percent.zero?
+      total = subtotal
+    else
+      coefficient = (subtotal / 100)
+      tax_sum = coefficient * tax_percent
+      total = subtotal + tax_sum
+    end
+    total
   end
 
   private
@@ -16,5 +30,13 @@ class Order < ApplicationRecord
 
   def update_subtotal
     self[:subtotal] = subtotal
+  end
+
+  def update_total
+    self[:total] = total
+  end
+
+  def update_total_cents
+    self[:total_cents] = total * 100.round
   end
 end
