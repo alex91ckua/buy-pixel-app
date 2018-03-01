44892740759b5338bf88ca0c3561ccec6077abf0
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-01 15:02:23 +0200

    total_cents added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301091351) do
+ActiveRecord::Schema.define(version: 20180301114525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180301091351) do
     t.bigint "order_status_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total_cents"
     t.index ["order_status_id"], name: "index_orders_on_order_status_id"
   end
 
