e01b1f0b8428537d4bf77743e0a2cbfd7dee45fd
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-01 22:09:13 +0200

    validations added, update product info on success purchase

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,22 @@
 class ChargesController < ApplicationController
-
-  def new
-  end
+  before_action :cart_exist, only: [:new, :create]
+  before_action :validate_stock, only: [:new, :create]
 
   def create
 
-    customer = Stripe::Customer.create(
-        :email => params[:stripeEmail],
-        :source  => params[:stripeToken]
+    customer = Stripe::Customer.create( :email => params[:stripeEmail],
+                                        :source  => params[:stripeToken]
     )
 
-    charge = Stripe::Charge.create(
-        :customer    => customer.id,
-        :amount      => current_order.total_cents, # Amount in cents
-        :description => 'Rails Stripe customer',
-        :currency    => 'aud'
-    )
+    p params
 
-    session[:success] = true
-    session[:transaction_id] = charge.id
+    charge = Stripe::Charge.create( :customer    => customer.id,
+                                    :amount      => current_order.total_cents, # Amount in cents
+                                    :description => 'Al-Ihsan Order',
+                                    :currency    => 'aud')
+    @transaction_id = charge.id
+    @billing_name = params[:stripeBillingName]
+    complete_order
     redirect_to charge_success_path
 
   rescue Stripe::CardError => e
@@ -28,6 +26,42 @@ class ChargesController < ApplicationController
 
   def success
     redirect_to root_path unless session[:success]
+  end
+
+  private
+
+  def complete_order
+    current_order.update_attributes(  :order_status_id => 2,
+                                      :transaction_id => @transaction_id,
+                                      :sponsor_name => @billing_name)
+    update_product_info
+    session.delete(:order_id)
+    session[:success] = true
+  end
+
+  def update_product_info
+    current_order.order_items.each do |oi|
+      product = Product.find_by_id(oi.product_id)
+      # let's decrease stock qty and change money_collected of products
+      in_stock = product.in_stock - oi.quantity
+      item_purchased = product.item_purchased + oi.quantity
+      money_collected = current_order.subtotal + product.money_collected
+      product.update_attributes(  :in_stock => in_stock,
+                                  :money_collected => money_collected,
+                                  :item_purchased => item_purchased)
+    end
+  end
+
+  def cart_exist
+    redirect_to root_path unless session[:order_id]
+  end
+
+  def validate_stock
+    current_order.order_items.each do |oi|
+      product = Product.find_by_id(oi.product_id)
+      in_stock = product.in_stock - oi.quantity
+      redirect_to cart_path, :flash => { :danger => "You can't purchase '#{product.name}', there is only #{product.in_stock} left!" } if in_stock < 0
+    end
   end
 
 end
