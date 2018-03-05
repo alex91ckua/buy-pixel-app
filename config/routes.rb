f83eba919d43342ec829d6ff6d8c619a48b44859
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-05 13:25:39 +0200

    static pages routes added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resource :cart, only: [:show, :create]
   root 'home#index'
   resource :charge, only: [:new, :create]
+  resource :privacy_policy, :path => 'privacy-policy', only: [:show]
+  resource :terms_and_conditions, :path => 'terms', only: [:show]
   get 'charge/success', to: 'charges#success'
   post 'contact_us/create'
   get 'contact_us/status'
