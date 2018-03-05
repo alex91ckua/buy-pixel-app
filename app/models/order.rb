81dff5a5e9fe906d7a16e02eb239ccf99a6b45a2
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-05 14:13:18 +0200

    sheduler added

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :order_status
   has_many :order_items, :dependent => :destroy
+  enum order_status: { in_progress: 1, placed: 2, canceled: 3 }
   # before_validation :set_order_status, on: :create
   before_save :update_subtotal
   before_save :update_total
   before_save :update_total_cents
+  validate :validate_order_items
+
 
   def subtotal
     order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
@@ -22,7 +25,21 @@ class Order < ApplicationRecord
     total
   end
 
+  # destroy all in_progress orders which was created more than 1 day ago
+  def self.clean_in_progress_orders
+    Order.where("created_at < :day AND order_status_id = :in_progress", {:day => 1.day.ago, :in_progress => 1}).destroy_all
+  end
+
   private
+
+  def validate_order_items
+    order_items.each do |oi|
+        next if oi.valid?
+        oi.errors.full_messages.each do |msg|
+          errors.add(:base, "Item Error: #{msg}")
+        end
+      end
+  end
 
   def set_order_status
     self.order_status_id = 1
