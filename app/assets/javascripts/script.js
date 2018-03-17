f58102e7955d0bf64f527ed3ba184b2e9e679f68
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-17 16:01:06 +0200

    pixel image popup added

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -35,6 +35,24 @@ $j( document ).on('turbolinks:load', function() {
 
   drawPixels("#pixelsContainer");
 
+
+    // Popup for pixels image
+    $j('.buy-pixel__img').magnificPopup({
+        type:'image',
+        removalDelay: 500, //delay removal by X to allow out-animation
+        callbacks: {
+            beforeOpen: function() {
+                // just a hack that adds mfp-anim class to markup
+                this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+                this.st.mainClass = this.st.el.attr('data-effect');
+            },
+            open: function() {
+                $j(".mfp-figure  figure").html($j("#pixelsContainer").clone());
+            }
+        },
+        closeOnContentClick: true
+    });
+
 });
 
 
