d19ed68b9e91a2615ae87a10420d480b3db04aee
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-24 17:07:33 +0200

    active admin editable fields added

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,13 +1,14 @@
 ActiveAdmin.register Product do
   actions :all, except: [:destroy, :new, :create]
   remove_filter :order_items, :updated_at
-  permit_params :name, :active, :price, :in_stock, :from_date, :to_date, :product_image
+  permit_params :name, :active, :price, :money_goal, :in_stock, :from_date, :to_date, :product_image
 
   form(:html => { :multipart => true }) do |f|
     f.inputs "Product" do
       f.input :name
       f.input :active
       f.input :price
+      f.input :money_goal
       f.input :in_stock
       f.input :from_date
       f.input :to_date
