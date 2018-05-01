03d34f5538d0521fb3b7c37c4abe9cb1ba106039
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-05-02 00:20:06 +0300

    allow to delete orders rename custom_pixel_percentage field

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Order do
 
-  actions :all, except: [:destroy, :new, :create]
+  actions :all
   remove_filter :order_items, :updated_at
   permit_params :sponsor_name, :order_status
 
